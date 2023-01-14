b327228c8d9aba4478741bb4a25aafafb51ce107
jesselizcb <choque101108@gmail.com>
2023-01-13 19:57:25 -0500

    add vuetify

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,33 +1,40 @@
 <template>
   <div>
-    <h2>Counter!</h2>
-    <p>{{contador}}<sup>2</sup> = {{ squareCounterComputed }}</p>
-    <p>{{contador}}<sup>2</sup> = {{ squareCounterComputed }}</p>
-    <p>{{contador}}<sup>2</sup> = {{ squareCounterComputed }}</p>
-    <p>{{contador}}<sup>2</sup> = {{ squareCounterComputed }}</p>
-    <p>{{contador}}<sup>2</sup> = {{ squareCounterComputed }}</p>
-    <p>{{contador}}<sup>2</sup> = {{ squareCounterComputed }}</p>
-    <div>
-      <button @click="increse">+1</button>
-      <button @click="decrease">-1</button>
-    </div>
+    <div class="d-flex flex-column align-center pt-10">
+      <h2>Counter!</h2>
+      <p>{{ contador }}<sup>2</sup> = {{ squareCounterComputed }}</p>
+      <p>{{ contador }}<sup>2</sup> = {{ squareCounterComputed }}</p>
+      <p>{{ contador }}<sup>2</sup> = {{ squareCounterComputed }}</p>
+      <p>{{ contador }}<sup>2</sup> = {{ squareCounterComputed }}</p>
+      <p>{{ contador }}<sup>2</sup> = {{ squareCounterComputed }}</p>
+      <p>{{ contador }}<sup>2</sup> = {{ squareCounterComputed }}</p>
+      <div>
+        <v-btn class="mx-2" fab dark small color="primary" @click="decrease">
+          <v-icon dark> mdi-minus </v-icon>
+        </v-btn>
+        <v-btn class="mx-2" fab dark small color="purple" @click="increse">
+          <v-icon dark> mdi-plus </v-icon>
+        </v-btn>
 
+        <!-- <button class="btn-inicial" @click="increse">+1</button>
+        <button class="btn-inicial" @click="decrease">-1</button> -->
+      </div>
+    </div>
   </div>
-  
 </template>
 
 <script>
 export default {
   name: "Counter",
-    data() {
-      return {
-        contador: 10
-      }
+  data() {
+    return {
+      contador: 10,
+    };
   },
   methods: {
     squareCounter() {
-      console.log('in square method')
-      return this.contador * this.contador
+      console.log("in square method");
+      return this.contador * this.contador;
     },
     increse() {
       this.contador++;
@@ -35,27 +42,25 @@ export default {
 
     decrease() {
       this.contador--;
-    }
-
+    },
   },
   computed: {
     squareCounterComputed() {
-      console.log('in computed')
-      return this.contador * this.contador
-    }
-  }
-    
-  }
+      console.log("in computed");
+      return this.contador * this.contador;
+    },
+  },
+};
 </script>
 
 <style scoped>
-button {
+.btn-inicial {
   border: 1px solid white;
-  border-radius: 10px;
-  margin:0 5px;
-  color:white;
-  cursor:pointer;
+  border-radius: 5px;
+  margin: 0 5px;
+  color: white;
+  cursor: pointer;
   padding: 5px 15px;
-  background-color:rgb(87, 165, 87)
+  background-color: rgb(87, 165, 87);
 }
 </style>
