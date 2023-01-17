a7d969143268695cf8e50aa338271a3fe6946546
jesselizcb <choque101108@gmail.com>
2023-01-16 19:35:42 -0500

    creacion de todo app

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,28 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="deep-purple lighten-2" dark class="d-flex justify-center">
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <h2>TODO APP</h2>
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
-      <Counter/>
+      <!-- <Counter/> -->
+      <Todo />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import Counter from './components/Counter.vue';
+import Counter from "./components/Counter.vue";
+import Todo from "./components/Todo";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     Counter,
+    Todo,
   },
 
   data: () => ({
